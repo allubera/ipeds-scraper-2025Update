d0e70b73e47b596483680f27fc23c32f5d96ead6
allubera <amber.lubera@gmail.com>
2025-02-17 15:44:21 -0700

    Add files via upload

---
+++ scripts/downloadStataDoFiles.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Download all IPEDS  Complete Do Files for a given set of years
+Extract and keep final/revised versions
+Make a json specifying columns in each data file
+Hannah Recht, 04-04-16
+
+The do files contain the labels for the varaible values.  
+Stata files do not have a 'save' function, so we will need to run each script and then save the var labels and definitions to get a crosswalk between dummy values and their actual string definitions.
+"""
+
+from urllib.request import urlopen
+import json
+import zipfile
+import os
+import csv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("start", help="start year",
+                    type=int)
+parser.add_argument("stop", help="stop year",
+                    type=int)
+args = parser.parse_args()
+
+# Import json of available files, created in scraper.py
+with open('data/ipedsfiles.json') as fp:
+    allfiles = json.load(fp)
+
+# Download all the data in given years
+def downloadData(start, stop):
+    print("*****************************")
+    print("Downloading do files")
+    print("*****************************")
+    for i in range(start,stop):
+        print("Downloading " + str(i) + " do files")
+        # Make directory for the raw files - one per year
+        if not os.path.exists('dofiles/' + str(i) + '/'):
+            os.makedirs('dofiles/' + str(i) + '/')
+        # Download all the files in the json
+        for f in allfiles:
+            if(f['year']==i):
+                # URL to download
+                url = f['dourl']
+                # dataset file name (XXXX.zip)
+                urlname = url.split("http://nces.ed.gov/ipeds/datacenter/data/",1)[1]
+                rd = urlopen(url)
+                saveurl = "dofiles/" + str(i) +'/' + urlname
+                # Save the zip files
+                with open(saveurl, "wb") as p:
+                     p.write(rd.read())
+                     p.close()
+
+                # Unzip .zips
+                zip_ref = zipfile.ZipFile(saveurl, 'r')
+                zip_ref.extractall("dofiles/" + str(i) +'/')
+                zip_ref.close()
+
+                # Remove zip file
+                os.remove("dofiles/" + str(i) +'/' + urlname)
+
+# Some datasets have been revised over time, so they'll download XXXX.csv and XXXX_rv.csv
+# We only want the revised version
+def removeDups(start, stop):
+    print("*****************************")
+    print("Removing duplicates")
+    print("*****************************")
+    for i in range(start,stop):
+        print("Removing " + str(i) + " duplicates")
+        files = os.listdir('dofiles/' + str(i) + '/')
+        # See how many files are in each year
+        # print([i,len(files)])
+        for file in files:
+            # file name minus '.csv'
+            name = file[:-4]
+            # If the file name ends in _rv, keep that one and delete the other (no _rv)
+            if(name[-3:] =='_rv'):
+                #print(name)
+                unrevised = name[:-3]
+                if(os.path.exists('dofiles/' + str(i) + '/' + unrevised + '.csv')):
+                    os.remove('dofiles/' + str(i) + '/' + unrevised + '.csv')
+                    print('Removed ' + unrevised)
+#                else:
+#                    print('no match ' + unrevised)
+
+downloadData(args.start, args.stop)
+removeDups(args.start, args.stop)
---
+++ scripts/makeMasterLabels.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 13:29:34 2025
+Using do files downloaded from IPEDS to get interpretable data.
+@author: amberlubera
+"""
+
+import os
+import re
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("start", help="start year", type=int)
+parser.add_argument("stop", help="stop year", type=int)
+args = parser.parse_args()
+
+def extract_and_compile_labels(start, stop, output_excel='data/labels.xlsx'):
+    print("*****************************")
+    print("Assembling master labels Dict")
+    print("*****************************")
+    
+    all_labels = []
+
+    for i in range(start, stop):
+        dict_path = f'dofiles/{i}/'
+        if not os.path.exists(dict_path):
+            continue
+
+        for file in os.listdir(dict_path):
+            if file.endswith(".do"):
+                print(f"Processing {i} {file}")
+                file_path = os.path.join(dict_path, file)
+
+                try:
+                    with open(file_path, 'r', encoding='utf-8') as f:
+                        do_file_content = f.read()
+                except UnicodeDecodeError:
+                    print(f"Warning: {file_path} contains non-UTF-8 characters. Trying alternative encoding.")
+                    with open(file_path, 'r', encoding='latin-1') as f:
+                        do_file_content = f.read()
+
+                # Extract label definitions
+                for match in re.finditer(r'label define\s+([a-zA-Z_][a-zA-Z0-9_]*)\s+(.*)', do_file_content):
+                    label_name = match.group(1)
+                    value_labels_str = match.group(2)
+
+                    # Extract value-label pairs
+                    for pair in re.findall(r'(\d+)\s+"([^"]*)"', value_labels_str):
+                        value, label = pair
+                        all_labels.append([i, file, label_name, int(value), label])
+
+    # Save to Excel file
+    df = pd.DataFrame(all_labels, columns=['year', 'filename', 'label_name', 'value', 'label'])
+    os.makedirs("data", exist_ok=True)  # Ensure 'data/' directory exists
+    df.to_excel(output_excel, index=False)
+
+    print(f"Labels successfully saved to {output_excel}")
+
+extract_and_compile_labels(args.start, args.stop)
+
+''' 
+*The following are the possible values for the item imputation field variables
+*A Not applicable
+*B Institution left item blank
+*C Analyst corrected reported value
+*D Do not know
+*G Data generated from other data values
+*H Value not derived - data not usable
+*J Logical imputation
+*K Ratio adjustment
+*L Imputed using the Group Median procedure
+*N Imputed using Nearest Neighbor procedure
+*P Imputed using Carry Forward procedure
+*R Reported
+*Z Implied zero
+''' 
+# I also found this note which we will need to think about. O noticed that this was the same in many files. 
+
+# Expected output:
+# {'gender': {1: 'Male', 2: 'Female'}, 'age_group': {1: '18-25', 2: '26-35', 3: '36+'}, 'education': {1: 'High School', 2: "Bachelor's", 3: "Master's", 4: 'PhD'}}
